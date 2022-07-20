62177b19cace17ba80549f57b9794906ce541306
Ben Pitroff <dev@chicken1991.anonaddy.com>
2022-07-19 23:22:52 -0700

    preliminary handlebars just for testings/route checking (#23)

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 const User = require('./User');
 const Location = require('./Location');
 // const Playtimes = require('./Playtime');
-// const favoriteLocation = require('./favoriteLocation');
+const favoriteLocation = require('./favoriteLocation');
 
-// User.hasMany(favoriteLocation, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(favoriteLocation, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-// favoriteLocation.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+favoriteLocation.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
 
-module.exports = { User, Location };
+module.exports = { User, Location, favoriteLocation };
