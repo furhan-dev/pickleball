3dfc3b82a1c2c18fe268f0727f34051366948e89
Furhan Samani <contact@furhan.dev>
2022-07-19 22:06:09 -0700

    remove favoriteLocation.js, change id to INTEGER, remove NULL default values

--- models/index.js
+++ models/index.js
@@ -1,16 +1,4 @@
 const Users = require('./User');
 const Locations = require('./Location');
-// const Playtimes = require('./Playtime');
-const favoriteLocation = require('./favoriteLocation');
 
-Users.hasMany(favoriteLocation, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-favoriteLocation.belongsTo(Users, {
-  foreignKey: 'user_id'
-});
-
-
-module.exports = { Users, Locations, favoriteLocation };
+module.exports = { Users, Locations };
