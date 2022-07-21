475422b578a6e75747ded8396c9275bc45092e11
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-07-20 17:42:47 -0700

    login works

---
+++ public/assets/script/login.js
@@ -0,0 +1,30 @@
+const loginForm = async (event) => {
+alert("script activated")
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+ 
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginForm);
