832e1b6b06f1282e3e8009175c5abe574a29f670
Furhan Samani <103493932+furhan-dev@users.noreply.github.com>
2022-07-25 11:26:06 -0700

    fix top margin and add history to browser when searching to enable back button (#62)

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,7 @@ const getCourts = async (event) => {
 
   event.preventDefault();
   const location = document.querySelector('#location').value.trim();
+  window.history.pushState(null, document.location.href);
   document.location.replace(`/search/${location}`);
 }
 
