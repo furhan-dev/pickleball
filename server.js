f8e7f2f2ee3b84e0ceb2b3b8b1cea279c926b617
Furhan Samani <contact@furhan.dev>
2022-07-25 09:55:37 -0700

    move images folder

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views/images')));
 
 app.use(routes);
 
