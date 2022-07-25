902efcc9a8f4aba199e9acc7a479e491d69478e2
Furhan Samani <contact@furhan.dev>
2022-07-25 10:25:27 -0700

    use middleware to serve favicon

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const favicon = require('serve-favicon');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
@@ -37,6 +38,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'images', 'favicon-180.png')));
 
 app.use(routes);
 
